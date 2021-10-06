792c1ea322eea6ce7ee46fe4e54c59e6e4823a4f
rajupangeni79 <rajupangeni1@gmail.com>
2021-10-06 12:38:32 -0500

    3rd commit

--- src/main/java/demoTest/GitTest2.java
+++ src/main/java/demoTest/GitTest2.java
@@ -1,5 +1,14 @@
 package demoTest;
 
 public class GitTest2 {
+	public static void main(String[] args) {
+		System.out.println("Git test");
+		System.out.println("Git test1");
+		System.out.println("Git test2");
+		
+	}
+	
+	
+	
 
 }
